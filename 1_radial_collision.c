7b6b6767a96bdc72f9273278d67f6164061e89b5
CaptnJayce <captn.dev@protonmail.com>
2025-03-02 23:35:46 +0000

    implemented Distance calculation

--- 1_radial_collision.c
+++ 1_radial_collision.c
@@ -1,11 +1,12 @@
 /*
  * Object with radius
- *   - Radius should turn green/red depending on if player is in or outside it
+ *   - Object should turn green/red depending on if player is in radius or not 
  *   - No colliders - use actual math
  *   - Explosion.pgn!!!!!
  */
 
 #include <raylib.h>
+#include <math.h>
 
 #define SCREEN_WIDTH 1280
 #define SCREEN_HEIGHT 720
@@ -24,7 +25,15 @@ struct Barrel {
   int h;
   int w;
   int radius;
+  bool playerNear;
 };
+
+float Distance(Vector2 Player, Vector2 Barrel) {
+    float dx = Barrel.x - Player.x;
+    float dy = Barrel.y - Player.y;
+
+    return sqrt(dx * dx + dy * dy);
+}
 
 int main() {
   InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "5 big booms");
@@ -41,10 +50,11 @@ int main() {
   // barrel structure
   struct Barrel b;
   b.pos = (Vector2){SCREEN_WIDTH - 400, SCREEN_HEIGHT / 2};
-  b.colour = RED;
+  b.colour = GREEN;
   b.h = 30;
   b.w = 20;
   b.radius = 120;
+  b.playerNear = false;
 
   while (WindowShouldClose() == false) {
     // update
@@ -61,8 +71,24 @@ int main() {
     BeginDrawing();
     ClearBackground(RAYWHITE);
     DrawRectangle(p.pos.x, p.pos.y, p.w, p.h, p.colour);
-    DrawRectangle(b.pos.x, b.pos.y, b.w, b.h, b.colour);
-    DrawCircleLines(b.pos.x + (b.w / 2), b.pos.y + (b.h / 2), b.radius, b.colour);
+
+    float dist = Distance(p.pos, b.pos);
+
+    if (dist <= b.radius) {
+      b.playerNear = true;
+    } else {
+      b.playerNear = false;
+    }
+
+    if (b.playerNear == false) {
+      DrawCircleLines(b.pos.x + (b.w / 2), b.pos.y + (b.h / 2), b.radius, b.colour);
+      DrawRectangle(b.pos.x, b.pos.y, b.w, b.h, b.colour);
+    }
+    if (b.playerNear == true) {
+      DrawCircleLines(b.pos.x + (b.w / 2), b.pos.y + (b.h / 2), b.radius, RED);
+      DrawRectangle(b.pos.x, b.pos.y, b.w, b.h, RED);
+    }
+
     EndDrawing();
   }
 
